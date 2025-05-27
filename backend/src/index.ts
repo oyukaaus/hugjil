bcdf44e335fbf290846770d7f340a8e999c25296
oyukaus <oyukaau@gmail.com>
2025-05-27 14:55:14 +0800

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
 
 import authRoutes from './routes/authRoutes';
 import chatRoutes from './routes/chatRoutes'; 
 import userRoutes from './routes/userRoutes'; 
 
 const app = express();
-const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(cors({
+  origin: 'http://103.41.112.99:3099'
+}));
 app.use(express.json());
 
 // Route registration
